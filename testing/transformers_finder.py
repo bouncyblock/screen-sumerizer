8ff0db7550915896b6697548397733d862c0ff0a
Will Kuntze <will.kuntze@gmail.com>
2026-01-07 22:22:54 -0500

    requirements update + removal of testing

--- testing/transformers_finder.py
+++
@@ -1,3 +0,0 @@
-import transformers
-print(transformers.__file__)
-print(transformers.__version__)
--- testing/tts.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-
-text = "Hello, this is a test of gTTS."
-
-tts = gTTS(text=text, lang="en")
-tts.save("output.mp3")
-
-print("Saved output.mp3")
